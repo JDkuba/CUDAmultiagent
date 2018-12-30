5851d1839a528bb338f88eda5bcd7bc9edd5d94f
JDkuba <jakub.dyczek@gmail.com>
2018-12-30 15:28:41 +0100

    ver0.02

--- src/engine.cu
+++ src/engine.cu
@@ -19,10 +19,27 @@ float rand_float(int b) {
 
 __global__ void move(agent *agents, agent *agents_out, int n_agents, int board_x, int board_y) {
     int ix = blockDim.x * blockIdx.x + threadIdx.x;
-    if (ix > n_agents * (n_agents - 1) / 2) return;
+    if (ix < n_agents) {
+        agents_out[ix] = agents[ix];
+    }
+    if (ix > n_agents * n_agents) return;
+    int first = ix / n_agents;
+    int second = ix % n_agents;
+//    if (first >= second) return;
+//    printf("%d %d\n", ix/n_agents, ix%n_agents);
+    //first, second to indeksy ktore bedzie sprawdzal dany watek
+
+
+    //todo od teraz jedno wielkie todo, tylko zrobione zeby jak kolwiek sie ruszali, na razie bez sprawdzania
+    if (ix < n_agents) {
+        printf("%d\n", ix);
+        agents_out[ix].move();
+    }
+
 }
 
 void run(int n_agents, int board_x, int board_y) {
+    float thickness = 1; //thickness of every agent
     agent* agents = new agent[n_agents];
     srand (time(NULL));
     for (int i = 0; i < n_agents; ++i) {
@@ -35,7 +52,7 @@ void run(int n_agents, int board_x, int board_y) {
     printf("\n");
 
     int block_size = 1024;
-    int grid_size = (n_agents * (n_agents - 1) / 2) / block_size + 1;     //number of pairs
+    int grid_size = (n_agents * n_agents) / block_size + 1;     //number of pairs
 
     agent *d_agents;
     agent *d_agents_out;
@@ -46,7 +63,7 @@ void run(int n_agents, int board_x, int board_y) {
 
     move << < grid_size, block_size >> > (d_agents, d_agents_out, n_agents, board_x, board_y);
 
-    gpuErrchk(cudaMemcpy(agents, d_agents, n_agents * sizeof(agent), cudaMemcpyDeviceToHost));
+    gpuErrchk(cudaMemcpy(agents, d_agents_out, n_agents * sizeof(agent), cudaMemcpyDeviceToHost));
 
     for (int i = 0; i < n_agents; ++i) {
         printf("%f %f %f %f %f\n", agents[i].x(), agents[i].y(), agents[i].vx(), agents[i].vy(), agents[i].max_speed());
