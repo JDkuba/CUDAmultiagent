3e51f49017adb6019e98afda107e3ae91db8bc6a
MacNems <macieknems@gmail.com>
2018-12-30 01:28:13 +0100

    Application: Write example animation

--- movement-simulator/src/main/java/controller/Controller.java
+++ movement-simulator/src/main/java/controller/Controller.java
@@ -1,7 +1,11 @@
 package controller;
 
+import abstractClasses.AbstractPlane;
 import javafx.stage.Stage;
+import utility.Position;
 import view.View;
+
+import java.util.ArrayList;
 
 public class Controller {
     private interfaces.View view;
@@ -10,6 +14,19 @@ public class Controller {
     }
 
     public void run() {
+        AbstractPlane plane = view.getPlane();
+        ArrayList<Position> agents = new ArrayList<>();
+        for (int i = 1; i <= 10; i++) {
+            agents.add(new Position(i*30, i*30));
+        }
+        plane.addAgents(agents);
 
+        plane.setOnMouseClicked(event -> {
+            for (int i = 1; i <= 10; i++) {
+                agents.get(i-1).setX(plane.getWidth()-i*30);
+                agents.get(i-1).setY(plane.getHeight()-i*30);
+            }
+            plane.moveAgents(agents);
+        });
     }
 }
