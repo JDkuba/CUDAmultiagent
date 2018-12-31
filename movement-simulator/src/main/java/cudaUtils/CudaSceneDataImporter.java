4e88b1f59f548889987c1d84d65d74cc32fe3e7b
Piotr Gaiński <dociebieaniuszlem@gmail.com>
2018-12-31 13:21:50 +0100

    Setup artifacts building

--- movement-simulator/src/main/java/cudaUtils/CudaSceneDataImporter.java
+++ movement-simulator/src/main/java/cudaUtils/CudaSceneDataImporter.java
@@ -6,12 +6,12 @@ import java.util.List;
 
 public class CudaSceneDataImporter {
 
-    static public final String METADATA_FILE_PATH = "path";
-    static public final String PATHS_FILE_PATH = "path2";
+    static public final String METADATA_FILE_PATH = "../../../../data/metadata.out";
+    static public final String PATHS_FILE_PATH = "../../../../data/agents_positions.out";
 
     static public CudaSceneMetadata getCudaSceneMetadata(){
         return null;
-    }
+    }me
 
     static public List<List<Position>> getAgentsPaths(){
         return null;
