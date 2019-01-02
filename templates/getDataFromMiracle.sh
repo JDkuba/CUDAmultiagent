e050de3d06ab641825f438b44b78e598c585e673
Piotr Gaiński <dociebieaniuszlem@gmail.com>
2019-01-02 23:13:20 +0100

    Add compression support to getData script

--- templates/getDataFromMiracle.sh
+++ templates/getDataFromMiracle.sh
@@ -1,8 +1,10 @@
 miracle='miracle'
 directory='/home/z1143051/cuda/MultiAgentSystem'
+archive='data.tar.bz2'
 
 mkdir -p data
 
-scp $miracle:$directory/*.out ./data/
-
+scp $miracle:$directory/$archive ./data/
+tar -jxvf ./data/$archive -C ./data
+rm ./data/$archive
 
